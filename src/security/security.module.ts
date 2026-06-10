e39f2310448a7eea84ace1419c6bc3b483573505
Rafael Rivero <rafalejandrorivero@gmail.com>
2026-06-09 21:46:55 -0400

    Feature[security-module]: Agregar ErrorRateLimitCounterMiddleware y configurar su uso en SecurityModule

--- src/security/security.module.ts
+++ src/security/security.module.ts
@@ -1,18 +1,26 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import type { ToolkitOptions } from '../core/interfaces/toolkit-options.interface';
 import { TOOLKIT_OPTIONS } from '../core/tokens';
 import { ErrorRateLimitGuard, HmacGuard } from './guards';
+import { ErrorRateLimitCounterMiddleware } from './middlewares/error-rate-limit-counter.middleware';
 
 @Module({
-  providers: [HmacGuard, ErrorRateLimitGuard],
+  providers: [HmacGuard, ErrorRateLimitGuard, ErrorRateLimitCounterMiddleware],
   exports: [HmacGuard, ErrorRateLimitGuard],
 })
-export class SecurityModule {
+export class SecurityModule implements NestModule {
   static forRoot(options: ToolkitOptions): DynamicModule {
     return {
       module: SecurityModule,
       providers: [{ provide: TOOLKIT_OPTIONS, useValue: options }],
       exports: [TOOLKIT_OPTIONS],
     };
+  }
+
+  configure(consumer: MiddlewareConsumer): void {
+    consumer.apply(ErrorRateLimitCounterMiddleware).forRoutes({
+      path: '*',
+      method: RequestMethod.ALL,
+    });
   }
 }
