9b83a3a8989296513911b536f9e2bdfd3ff34eb3
Rafael Rivero <rafalejandrorivero@gmail.com>
2026-06-10 10:18:36 -0400

    Feature: exportar interfaces y repositorios adicionales en el módulo OAuth

--- src/oauth/index.ts
+++ src/oauth/index.ts
@@ -3,3 +3,5 @@ export * from './oauth.service';
 export * from './commands';
 export * from './interfaces/oauth-token-request.interface';
 export * from './interfaces/oauth-token-response.interface';
+export * from './interfaces/oauth-client-repository.interface';
+export * from './repository';
