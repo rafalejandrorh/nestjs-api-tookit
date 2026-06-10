73e40feccf46ffddbdcecaa111687d48bb788ab5
Rafael Rivero <rafalejandrorivero@gmail.com>
2026-06-10 14:00:45 -0400

    Feature: limpiar exportaciones en index.ts para mejorar la organización

--- src/oauth/index.ts
+++ src/oauth/index.ts
@@ -1,7 +1,4 @@
 export * from './oauth.module';
-export * from './oauth.service';
-export * from './commands';
 export * from './interfaces/oauth-token-request.interface';
 export * from './interfaces/oauth-token-response.interface';
 export * from './interfaces/oauth-client-repository.interface';
-export * from './repository';
